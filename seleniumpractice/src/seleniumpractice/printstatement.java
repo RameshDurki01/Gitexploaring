a9553b6880add8e9ddae3686eee39608cb6f5e92
DurkiRamesh <ramesh.durki05@gmail.com>
2024-07-10 13:59:14 +0530

    first commit

---
+++ seleniumpractice/src/seleniumpractice/printstatement.java
@@ -0,0 +1,12 @@
+package seleniumpractice;
+
+public class printstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("print statement");
+
+	}
+
+}
